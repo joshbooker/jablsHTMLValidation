97a51ffaacf83a09ae3cf45a48803ff207b60147
Josh Booker <josh@joshuabooker.com>
2015-10-09 16:02:39 +0000

    Added Account, Contact & Common Screen Set

---
+++ jablsHTMLValidation/jablsHTMLValidation.HTMLClient/Screens/Accounts/ViewAccount.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewAccount.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.Account.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.Account." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
